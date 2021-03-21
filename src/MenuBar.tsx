a269c645850e1997a521d78cb1b56ef24f07566b
Esteban Amas <estebanamas@gmail.com>
2021-03-20 23:30:42 -0400

    MenuBar

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -1,14 +1,30 @@
+import { useState } from 'react';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import { Avatar } from '@material-ui/core';
 import { useDataLayerValue } from './DataLayer';
 import { Link, useHistory } from 'react-router-dom';
 
+// menu button
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 const MenuBar = () => {
   let history = useHistory();
 
   //@ts-ignore
   const [{ user }] = useDataLayerValue();
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className="menubar">
@@ -34,8 +50,25 @@ const MenuBar = () => {
         </Link>
       </div>
       <div className="menubar-user">
-        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
-        <h4>{user?.display_name}</h4>
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+        >
+          <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+          <h4>{user?.display_name}</h4>
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={handleClose}>Account</MenuItem>
+          <MenuItem onClick={handleClose}>Profile</MenuItem>
+          <MenuItem onClick={handleClose}>Log out</MenuItem>
+        </Menu>
       </div>
     </div>
   );
