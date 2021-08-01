52951691f25ead9c3568e56da3305ac1be06e8fc
Esteban Amas <estebanamas@gmail.com>
2021-08-01 14:32:23 -0400

    Axios, server index

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const SpotifyWebApi = require('spotify-web-api-node');
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+  const code = req.body.code;
+  console.log(code);
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000',
+    clientId: process.env.REACT_APP_SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_SPOTIFY_CLIENT_SECRET,
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
