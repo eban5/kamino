cc1a3872a455bef12d2325830a11a5d6488d9fb7
Esteban Amas <estebanamas@gmail.com>
2021-08-01 22:29:52 -0400

    useAuth updates

--- src/useAuth.ts
+++ src/useAuth.ts
@@ -1,26 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function useAuth(code: string) {
-  const [accessToken, setAccessToken] = useState<string>();
-  const [refreshToken, setRefreshToken] = useState<string>();
-  const [expiresIn, setExpiresIn] = useState<string>();
+  const [accessToken, setAccessToken] = useState<string>('');
+  const [refreshToken, setRefreshToken] = useState<string>('');
+  const [expiresIn, setExpiresIn] = useState<number>(0);
 
-  // let axiosConfig = {
-  //   headers:
-
-  // }
+  // Login
   useEffect(() => {
     axios
       .post('http://localhost:3001/login', {
         code,
       })
       .then((res) => {
-        console.log(res.data);
+        setAccessToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+        setExpiresIn(res.data.expiresIn);
+        window.history.pushState({}, '/');
       })
       .catch(() => {
-        
         window.location.href = '/';
       });
   }, [code]);
+
+  // Refresh Token
+  useEffect(() => {
+    if (!refreshToken || !expiresIn) return;
+    const interval = setInterval(() => {
+      axios
+        .post('http://localhost:3001/refresh', {
+          refreshToken,
+        })
+        .then((res) => {
+          // setAccessToken(res.data.accessToken);
+          // setRefreshToken(res.data.refreshToken);
+          // setExpiresIn(res.data.expiresIn);
+        })
+        .catch(() => {
+          window.location.href = '/';
+        });
+    }, (expiresIn - 60) * 1000);
+  }, [refreshToken, expiresIn]);
+  return accessToken;
 }
